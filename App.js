047937d91bc5b67e3f53080f40802b3e7315a5ef
Charly <cbarrionuevo@fidelatam.com>
2023-05-04 16:47:11 -0300

    cambios revisados

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const app = express();
 
-<<<<<<< HEAD
 const routeCronTester = require('./controllers/cronTester/cronTester');
 
 app.use('/cronTester',routeCronTester);
-app.listen(3000);
-=======
-const routeCronTester= require('./controllers/cronTester/cronTester');
-
-app.use('/cronTester',routeCronTester);
 
 app.listen(3000);
->>>>>>> 3562ce4a548cf414bfa2211200968fcf245cc71a
