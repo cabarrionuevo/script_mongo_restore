43e2f21bebf51bfbbd56241b51c2517612e83096
Gaston Leegstra <gleegstra@fidelatam.com>
2023-05-09 15:44:48 -0300

    se agrega cambios con nuevas variables de entorno, libreria body-parser y recupero de datos del request

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const dbRoutes = require('./routes/db_routes');
+
+app.use(bodyParser.urlencoded({
+    extended:true
+})); 
 
 app.set('view engine','pug');
 
--- controllers/db_controller.js
+++ controllers/db_controller.js
@@ -26,32 +26,34 @@ module.exports = {
     new: async function (req,res){
         res.render('config');
     },
-    download: async function (req,res) {
-        // let accessKey = req.body.access_key;
-        // let secretKey = req.body.secret_key;
-        // let path = req.body.path_local;
-        // let radioBtnValue = req.body.backup_selection;
-        
-        console.log(req.body);
-
-        
+    download: async function (req,res) {      
         try {
+            
             //credenciales las pide por formulario
             const credentials = {
-                accessKeyId: config.AWS_ACCESS_KEY,
-                secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
+                accessKeyId: req.body.access_key,
+                secretAccessKey: req.body.secret_key,
             };
 
             //se recibe por formulario            
-            const filePath =    config.PATH_LOCAL;
+            const filePath =    req.body.path_local;
 
-            //variables bucket se defiene en archivo env depende del checkbox elegido
-            let inputFolder =   config.FOLDER_IN_BUCKET
-            const filename =    config.FILE_IN_BUCKET;
+            let inputFolder, filename = "";
+
+            if (req.body.backup_selection === "mongo") {
+                //variables bucket se defiene en archivo env depende del checkbox elegido
+                inputFolder =   config.FOLDER_IN_BUCKET_MONGO;
+                filename    =   config.FILE_IN_BUCKET_MONGO;
+            }else{
+                inputFolder =   config.FOLDER_IN_BUCKET_POSTGRES;
+                filename    =   config.FILE_IN_BUCKET_POSTGRES;
+            }
+
+
             //se tiene que crear 2 variables mas para la otra base a descargar
 
             const params = {
-                Bucket: config.BUCKET_BKP_MONGO,
+                Bucket: config.BUCKET_BKP,
                 Key: inputFolder + filename,
             };
             
@@ -69,7 +71,7 @@ module.exports = {
             outStream.on('finish',()=>{
                 console.log('Objeto descargado y guardado en:', downloadPath);
                 res.send(`Objeto descargado y guardado en: ${downloadPath}`);
-            });                           
+            });                         
 
         } catch (error) {
             console.log(error);
